af783cc6ed2bdbc27a53b297c48c8ad541f1bff5
anomic84@gmail.com <spencercarlson@Spencers-MacBook-Pro-2.local>
2023-03-18 16:25:37 -0700

    created controllers' psuedocodes with basic start points

---
+++ controllers/thought-controller.js
@@ -0,0 +1,20 @@
+const { User, Thought} = require('../models');
+
+const thoughtController {
+
+    // getAllThoughts(req, res) Thought.find({})
+
+    //getThoughtById({ params }, res) Thought.findOne({ _id: params.id })
+
+    // createThought({ body }, res) Thought.create(body)
+
+    // updateThought({ params, body }, res) Thought.findOneAndUpdate({ _id: params.id }, body)
+
+    // deleteThought({ params }, res) Thought.findOneAndDelete({ _id: params.id })
+
+    // create reaction({ params, body }, res) Thought.findOneAndUpdate({_id: params.thoughtId}}
+
+    // deleteReaction({ params }, res) Thought.findOneAndUpdate({ _id: params.thoughtId })
+}
+
+module.exports = thoughtController
---
+++ controllers/user-controller.js
@@ -0,0 +1,20 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+
+    // getAllUser(req,res) User.find({})
+
+    // getUserById({ params }, res) User.findOne({ _id: params.id})
+
+    // createUser({ body }),res) User.create(body)
+
+    // updateUser({ params, body }, res) User.findOneAndUpdate({ _id: params.id}, body)
+
+    // deleteUser({ params }, res) Thought.deleteMany({ userId. params.id}) THEN User.findOneandDelete({ userId: params.id })
+
+    // addFriend({ params }, res) User.findOneAndUpdate({ _id: params.userId }
+
+    // deleteFriend({ params }, res) User.findOneAndUpdate({ _id: params.userId }
+};
+
+module.exports = userController
