18daa1dcc495b39e165f442e3f48e4c0650edf9c
anomic84@gmail.com <spencercarlson@Spencers-MacBook-Pro-2.local>
2023-03-19 15:46:42 -0700

    added a delete user method

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -4,50 +4,74 @@ const userController = {
 
     getAllUser(req, res) {
         User.find({})
-          .select('-__v')
-          .sort({ _id: -1 })
-          .then(dbUserData => res.json(dbUserData))
-          .catch(err => {
-            console.log(err);
-            res.sendStatus(400);
-          });
-      },
+            .select('-__v')
+            .sort({ _id: -1 })
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
 
-      getUserById({ params }, res) {
+    getUserById({ params }, res) {
         User.findOne({ _id: params.id })
-        // populates user's thoughts into JSON
-          .populate({
-            path: 'thoughts',
-            select: '-__v'
-          })
-        //   populates user's friends into JSON
-          .populate({
-            path: 'friends',
-            select: '-__v'
-          })
-          .then(dbUserData => {
-            if (!dbUserData) {
-              res.status(404).json({ message: 'No User found with this id!' });
-              return;
-            }
-            res.json(dbUserData);
-          })
-          .catch(err => {
-            console.log(err);
-            res.sendStatus(400);
-          });
-      },
+            // populates user's thoughts into JSON
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            //   populates user's friends into JSON
+            .populate({
+                path: 'friends',
+                select: '-__v'
+            })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No User found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
 
-      createUser({ body }, res) {
+    createUser({ body }, res) {
         User.create(body)
-          .then(dbUserData => res.json(dbUserData))
-          .catch(err => res.json(err));
-      },
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.json(err));
+    },
 
-    // updateUser({ params, body }, res) User.findOneAndUpdate({ _id: params.id}, body)
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No User found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+    },
 
-    // deleteUser({ params }, res) Thought.deleteMany({ userId. params.id}) THEN User.findOneandDelete({ userId: params.id })
-
+    deleteUser({ params }, res) {
+        // deletes all of users thoughts/reactions and other data first
+        Thought.deleteMany({ userId: params.id })
+            .then(() => {
+                // deletes user
+                User.findOneAndDelete({ userId: params.id })
+                    .then(dbUserData => {
+                        if (!dbUserData) {
+                            res.status(404).json({ message: 'No User found with this id!' });
+                            return;
+                        }
+                        res.json(dbUserData);
+                    });
+            })
+            .catch(err => res.json(err));
+    },
     // addFriend({ params }, res) User.findOneAndUpdate({ _id: params.userId }
 
     // deleteFriend({ params }, res) User.findOneAndUpdate({ _id: params.userId }
