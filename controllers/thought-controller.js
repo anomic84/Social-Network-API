7985a0b4562f31a92ad81cd79b7096c2a9995733
anomic84@gmail.com <spencercarlson@Spencers-MacBook-Pro-2.local>
2023-03-18 18:46:25 -0700

    finished get all thoughts

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,8 +1,25 @@
-const { User, Thought} = require('../models');
+const { User, Thought } = require('../models');
 
 const thoughtController {
 
-    // getAllThoughts(req, res) Thought.find({})
+    getAllThoughts(req, res) {
+        Thought.find({})
+            // populate lets us fill out a section by referenceing another schema/model
+            .populate({
+                path: 'reactions',
+                // __v is version key, and the - before it excludes it
+                select: '-__v'
+            })
+            .select('-__v')
+            // sorts be descending order
+            .sort({ _id: -1 })
+            // turns data into "dbThoughtData" and turns it into JSON data
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+              });
+    }
 
     //getThoughtById({ params }, res) Thought.findOne({ _id: params.id })
 
